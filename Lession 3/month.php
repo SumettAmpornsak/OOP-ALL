49ccac2330e36648dc321543db73b0a8b2f856a7
Sumett studio <90433375+SumettAmpornsak@users.noreply.github.com>
2023-07-18 12:14:36 +0700

    Lession 3

---
+++ Lession 3/month.php
@@ -0,0 +1,17 @@
+<?php
+// สร้างอาร์เรย์ของเดือนในภาษาอังกฤษ
+    $months = array('January','February','March','April','May','June','July','August','September','October','November','December');
+
+
+//แสดงผลอาร์เรย์โดยใช้โครงสร้างการทำงานซ้ำ
+    for($i=0;$i<12;$i++)
+        echo "เดือนที่"," " ,$i+1 ," ","คือเดือน"," ",$months[$i],"<br>";
+
+//echo $months[$i]," ","<br>";
+
+
+
+
+
+
+?>
---
+++ Lession 3/show.php
@@ -0,0 +1,13 @@
+<?php
+
+$fname=$_POST["fname"];
+$lname=$_POST["lname"];
+$uni=$_POST["uni"];
+
+
+
+echo "Hello $fname $lname from $uni to Nakhon Pathom.";
+
+
+
+?>
